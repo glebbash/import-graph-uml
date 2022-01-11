13acd798bb20c557e8764d7801b348f4908b473d
Gleb Bashkatov <glebbash@gmail.com>
2022-01-11 10:09:35 +0200

    fix(ci): add plantuml jar to package

--- src/parse-args.ts
+++ src/parse-args.ts
@@ -5,7 +5,10 @@ const argsParser = yargs([])
   .options({
     src: { type: 'string', demandOption: true },
     out: { type: 'string', default: 'out.puml' },
-    plantUmlJar: { type: 'string', default: resolve(__dirname, '../plantuml-1.2021.16.jar') },
+    plantUmlJar: {
+      type: 'string',
+      default: resolve(__dirname, '../assets/plantuml-1.2021.16.jar'),
+    },
     include: { type: 'string', default: 'src/**/*.ts' },
     builder: { type: 'string', default: resolve(__dirname, './build-module-graph') },
   })
